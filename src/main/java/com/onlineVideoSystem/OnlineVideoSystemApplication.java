d675097741709b209716525ef1b07993d5a102fd
Luke-lgc <2857179139@qq.com>
2021-12-12 18:33:30 +0800

    Spring项目初始化

---
+++ src/main/java/com/onlineVideoSystem/OnlineVideoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.onlineVideoSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVideoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineVideoSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlineVideoSystem/OnlineVideoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineVideoSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineVideoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
